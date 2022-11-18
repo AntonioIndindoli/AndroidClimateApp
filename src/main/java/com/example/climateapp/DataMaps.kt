91494d95fad3e8ec21eaaf1e37b43722c636101f
AntonioIndindoli <50554331+AntonioIndindoli@users.noreply.github.com>
2022-11-17 18:42:36 -0800

    implemented bitmap IO

--- src/main/java/com/example/climateapp/DataMaps.kt
+++ src/main/java/com/example/climateapp/DataMaps.kt
@@ -2,13 +2,26 @@ package com.example.climateapp
 
 import com.example.climateapp.R
 import android.content.Context
+import android.graphics.Bitmap.createScaledBitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color.*
 import java.security.AccessController
 
 class DataMaps (context : Context){
-    val icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.test)
-    val pixels = IntArray(icon.width * icon.height)
-    fun makeArray (){
-        icon.getPixels(pixels, 0, 0, 0, 0, icon.width, icon.height)
+    var landMass = BitmapFactory.decodeResource(context.getResources(), R.drawable.albedo)
+
+    fun getLandData (): IntArray {
+        landMass = createScaledBitmap(landMass, 40, 20, false)
+        val pixels = IntArray(landMass.width * landMass.height)
+        landMass.getPixels(pixels, 0, landMass.width, 0, 0, landMass.width, landMass.height)
+
+        var sum = 0;
+        for (i in 0 until pixels.size) {
+            pixels[i] = green(pixels[i]);
+            sum += pixels[i];
+            System.out.println(pixels[i]);
+        }
+        System.out.println(sum/800);
+        return pixels
     }
 }
--- src/main/java/com/example/climateapp/MainActivity.kt
+++ src/main/java/com/example/climateapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.climateapp
 
+import android.graphics.Color.*
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -11,11 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.climateapp.ui.theme.ClimateAppTheme
 import kotlinx.coroutines.Job
@@ -25,30 +26,55 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val getData = DataMaps(baseContext)
+        val landMass = getData.getLandData()
+
         super.onCreate(savedInstanceState)
         setContent {
             ClimateAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    DefaultPreview()
+                    RenderMap(landMass)
+
                 }
             }
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun Greeting(Array: Array<IntArray>) {
+    fun DisplayMap(Array: Array<IntArray>) {
+
+    val degree8 = Color(red = 0.60f, green = 0.20f, blue = 0.1f, alpha = 0.5f)
+    val degree7 = Color(red = 0.70f, green = 0.20f, blue = 0.15f, alpha = 0.5f)
+    val degree6 = Color(red = 0.80f, green = 0.263f, blue = 0.212f, alpha = 0.5f)
+    val degree5 = Color(red = 0.957f, green = 0.263f, blue = 0.212f, alpha = 0.5f)
+    val degree4 = Color(red = 1.0f, green = 0.341f, blue = 0.133f, alpha = 0.5f)
+    val degree3 = Color(red = 1.0f, green = 0.596f, blue = 0.0f, alpha = 0.5f)
+    val degree2 = Color(red = 1.0f, green = 0.757f, blue = 0.027f, alpha = 0.5f)
+    val degree1 = Color(red = 1.0f, green = 0.922f, blue = 0.231f, alpha = 0.5f)
+    val Empty = Color(red = 1.0f, green = 0.922f, blue = 0.231f, alpha = 0f)
+
     Column() {
         for (i in 0 until Array.size) {
             Row {
                 for (j in 0 until Array[i].size) {
-                    if (Array[i][j] == 2) {
-                        ExampleBox(shape = RectangleShape, color = Red)
-                    }
-                    else{
-                        ExampleBox(shape = RectangleShape, color = Blue)
+                    when (Array[i][j]) {
+                        0 -> ClimateBox(shape = RectangleShape, color = Empty)
+                        1 -> ClimateBox(shape = RectangleShape, color = degree1)
+                        2 -> ClimateBox(shape = RectangleShape, color = degree2)
+                        3 -> ClimateBox(shape = RectangleShape, color = degree3)
+                        4 -> ClimateBox(shape = RectangleShape, color = degree4)
+                        5 -> ClimateBox(shape = RectangleShape, color = degree5)
+                        6 -> ClimateBox(shape = RectangleShape, color = degree6)
+                        7 -> ClimateBox(shape = RectangleShape, color = degree7)
+                        8 -> ClimateBox(shape = RectangleShape, color = degree8)
+                        else -> {
+                            ClimateBox(shape = RectangleShape, color = degree8)
+                        }
                     }
                 }
             }
@@ -56,8 +82,9 @@ fun Greeting(Array: Array<IntArray>) {
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun ExampleBox(shape: Shape, color: androidx.compose.ui.graphics.Color){
+fun ClimateBox(shape: Shape, color: Color){
     Column(modifier = Modifier.wrapContentSize(Alignment.Center)) {
         Box(
             modifier = Modifier
@@ -68,9 +95,9 @@ fun ExampleBox(shape: Shape, color: androidx.compose.ui.graphics.Color){
     }
 }
 
-@Preview(showBackground = true)
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun DefaultPreview() {
+fun RenderMap(landMass: IntArray) {
     val colorGrid = Array(10) { IntArray(20) }
     for (i in 0 until colorGrid.size) {
         for (j in 0 until colorGrid[i].size) {
@@ -79,29 +106,30 @@ fun DefaultPreview() {
     }
 
     ClimateAppTheme {
-        startUpdates(colorGrid)
-        Greeting(colorGrid)
+        startUpdates(colorGrid, landMass)
+        DisplayMap(colorGrid)
     }
 }
 val scope = MainScope() // could also use an other scope such as viewModelScope if available
 var job: Job? = null
 
-fun startUpdates(Array: Array<IntArray>) {
+fun startUpdates(Array: Array<IntArray>, landMass: IntArray) {
     stopUpdates()
     job = scope.launch {
         while(true) {
-            updateMap(Array)
+            updateMap(Array, landMass)
             delay(1000)
         }
     }
 }
 
-fun updateMap(Array: Array<IntArray>) {
+fun updateMap(Array: Array<IntArray>, landMass: IntArray) {
     for (i in 0 until Array.size) {
         for (j in 0 until Array[i].size) {
             Array[i][j] = 2 //just an example
         }
     }
+    //System.out.println("test");
 }
 
 fun stopUpdates() {
